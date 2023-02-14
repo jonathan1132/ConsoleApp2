ca7771585a189f21175fe0b275d525797c76fcae
opilane <opilane@HP-CZC0359NNV>
2023-02-14 09:39:57 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Dictionary<int, string>
+            {
+                {1, "asd" },
+                {2, "ok" },
+            };
+
+            result.ToList().ForEach(x =>
+            Console.WriteLine($"{x.Key} - {x.Value}"));
+        }
+    }
+}
